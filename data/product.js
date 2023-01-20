a30e7223f4bd1654e60cf1c714d7412e1bc7c18d
Goldy Tanjung Wijaya <goldywijaya@gmail.com>
2023-01-20 22:45:59 +0700

    add product

--- data/product.js
+++ data/product.js
@@ -14,80 +14,67 @@ export const product = [
         price:360000
     },
     {
-        id:'w2',
-        name:'Quley Construction Template Kit',
-        img:'/images/quley.jpg',
-        link:'https://quley.websitesimple.id',
-        price:435000
-    },
-    {
-        id:'w3',
-        name:'Startio SaaS Template Kit',
-        img:'/images/startio.png',
-        link:'https://startio.websitesimple.id',
-        price:360000
-    },
-    {
         id:'w4',
-        name:'Sinai SaaS Template Kit',
-        img:'/images/sinai.jpg',
-        link:'https://sinai.websitesimple.id',
-        price:360000
-    },
-    {
-        id:'w5',
-        name:'Architude Construction Template Kit',
-        img:'/images/architude.jpg',
-        link:'https://architude.websitesimple.id',
-        price:360000
-    },
-    {
-        id:'w6',
-        name:'Quwa Personal Profile Template Kit',
-        img:'/images/quwa.jpg',
-        link:'https://quwa.websitesimple.id',
-        price:360000
-    },
-    {
-        id:'w7',
         name:'Dynamix Construction Template Kit',
         img:'/images/dynamix.jpg',
         link:'https://dynamix.websitesimple.id',
         price:400000
     },
     {
-        id:'w8',
-        name:'Danish Personal Profile Template Kit',
-        img:'/images/danish.png',
-        link:'https://danish.websitesimple.id',
-        price:300000
-    },
-    {
-        id:'w9',
-        name:'Xsap SaaS Template Kit',
-        img:'/images/xsap.jpg',
-        link:'https://xsap.websitesimple.id',
-        price:360000
-    },
-    {
-        id:'w10',
-        name:'Balton Construction Template Kit',
-        img:'/images/balton.jpg',
-        link:'https://balton.websitesimple.id',
-        price:435000
-    },
-    {
-        id:'w11',
+        id:'w5',
         name:'Blog4 Template Kit',
         img:'/images/blog4.jpg',
         link:'https://blog4.websitesimple.id',
         price:435000
     },
     {
-        id:'w12',
-        name:'Dustro Construction Template Kit',
-        img:'/images/dustro.png',
-        link:'https://dustro.websitesimple.id',
-        price:300000
+        id:'w2',
+        name:'Sinai SaaS Template Kit',
+        img:'/images/sinai.jpg',
+        link:'https://sinai.websitesimple.id',
+        price:360000
     },
+    {
+        id:'w3',
+        name:'Architude Construction Template Kit',
+        img:'/images/architude.jpg',
+        link:'https://architude.websitesimple.id',
+        price:360000
+    },
+    
+    // {
+    //     id:'w6',
+    //     name:'Quley Construction Template Kit',
+    //     img:'/images/quley.jpg',
+    //     link:'https://quley.websitesimple.id',
+    //     price:435000
+    // },
+    // {
+    //     id:'w7',
+    //     name:'Startio SaaS Template Kit',
+    //     img:'/images/startio.png',
+    //     link:'https://startio.websitesimple.id',
+    //     price:360000
+    // },
+    // {
+    //     id:'w8',
+    //     name:'Quley Construction Template Kit',
+    //     img:'/images/quley.jpg',
+    //     link:'https://quley.websitesimple.id',
+    //     price:435000
+    // },
+    // {
+    //     id:'w9',
+    //     name:'Startio SaaS Template Kit',
+    //     img:'/images/startio.png',
+    //     link:'https://startio.websitesimple.id',
+    //     price:360000
+    // },
+    // {
+    //     id:'w10',
+    //     name:'Balton Construction Template Kit',
+    //     img:'/images/balton.jpg',
+    //     link:'https://balton.websitesimple.id',
+    //     price:435000
+    // },
 ]
